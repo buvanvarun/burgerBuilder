58b8311c42c2a3528e9189c585fa4e917f995759
R P Buvan Varun <buvanvarun.acad@gmail.com>
2021-02-20 23:47:07 +0530

    added buttons

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import classes from "./Toolbar.css";
+const toolbar =(props)=>(
+    <header className={classes.Toolbar}>
+        menu logo nav(contians navigation links)
+    </header>
+)
+
+export default toolbar;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -65,6 +65,10 @@ class BurgerBuilder extends Component {
         this.setState({purchasing:false});
     }
 
+    purchaseContinueHandler=()=>{
+        alert("You continue!");
+    }
+
     render(){
         const disabledInfo={
             ...this.state.ingredients
@@ -82,7 +86,7 @@ class BurgerBuilder extends Component {
         return(
             <Auxiliary>
                 <Modal modalClosed={this.purchaseCancelHandler} show={this.state.purchasing} >
-                    <OrderSummary ingredients={this.state.ingredients}/>
+                    <OrderSummary price={this.state.totalPrice} purchaseCancled={this.purchaseCancelHandler} purchaseContinued={this.purchaseContinueHandler} ingredients={this.state.ingredients}/>
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls
